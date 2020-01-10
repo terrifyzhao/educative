1c69da162fc0404238bf6451744440e348f87722
terrifyzhao <zjiuzhou@gmail.com>
2020-01-10 17:26:54 +0800

    Create diff_ways_to_evaluate_expression.py

---
+++ 10_subsets/diff_ways_to_evaluate_expression.py
@@ -0,0 +1,15 @@
+def diff_ways_to_evaluate_expression(input):
+    result = []
+    # TODO: Write your code here
+    return result
+
+
+def main():
+    print("Expression evaluations: " +
+          str(diff_ways_to_evaluate_expression("1+2*3")))
+
+    print("Expression evaluations: " +
+          str(diff_ways_to_evaluate_expression("2*3-4-5")))
+
+
+main()
