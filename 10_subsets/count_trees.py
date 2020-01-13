e17cd68158444e94979e51445481733aecd533e1
terrifyzhao <zjiuzhou@gmail.com>
2020-01-13 15:35:59 +0800

    add code

---
+++ 10_subsets/count_trees.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def count_trees(n):
+    if n<=1:
+        return 1
+
+
+
+
+    return count
+
+
+def main():
+    print("Total trees: " + str(count_trees(2)))
+    print("Total trees: " + str(count_trees(3)))
+
+
+main()
--- 10_subsets/diff_ways_to_evaluate_expression.py
+++ 10_subsets/diff_ways_to_evaluate_expression.py
@@ -1,6 +1,25 @@
 def diff_ways_to_evaluate_expression(input):
     result = []
-    # TODO: Write your code here
+
+    # 全是数字
+    if '+' not in input and '-' not in input and '*' not in input:
+        result.append(int(input))
+    else:
+        for i in range(len(input)):
+            c = input[i]
+
+            if not c.isdigit():
+                left_part = diff_ways_to_evaluate_expression(input[:i])
+                right_part = diff_ways_to_evaluate_expression(input[i + 1:])
+                for p1 in left_part:
+                    for p2 in right_part:
+                        if c == '+':
+                            result.append(p1 + p2)
+                        elif c == '-':
+                            result.append(p1 - p2)
+                        elif c == '*':
+                            result.append(p1 * p2)
+
     return result
 
 
