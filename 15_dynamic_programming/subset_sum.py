d4bfe79a5a6c33c0545645d7c6ba17bc7bcb65c1
terrifyzhao <zjiuzhou@gmail.com>
2020-01-19 15:21:47 +0800

    add code

--- 15_dynamic_programming/subset_sum.py
+++ 15_dynamic_programming/subset_sum.py
@@ -13,10 +13,25 @@ def can_partition(num, sum):
     return dp[sum]
 
 
+def can_partition2(num, sum):
+    dp = [False for _ in range(sum + 1)]
+    dp[0] = True
+
+    # for s in range(sum + 1):
+    #     dp[s] = s == num[0]
+
+    for i in range(len(num)):
+        for s in range(sum, -1, -1):
+            if not dp[s] and s >= num[i]:
+                dp[s] = dp[s - num[i]]
+
+    return dp[-1]
+
+
 def main():
-    print("Can partition: " + str(can_partition([1, 2, 3, 7], 6)))
-    print("Can partition: " + str(can_partition([1, 2, 7, 1, 5], 10)))
-    print("Can partition: " + str(can_partition([1, 3, 4, 8], 6)))
+    print("Can partition: " + str(can_partition2([1, 2, 3, 7], 6)))
+    print("Can partition: " + str(can_partition2([1, 2, 7, 1, 5], 10)))
+    print("Can partition: " + str(can_partition2([1, 3, 4, 8], 6)))
 
 
 main()
