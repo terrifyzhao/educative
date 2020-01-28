731cc9ad0ae23c3ca28612cb80cd584134d188a2
terrifyzhao <zjiuzhou@gmail.com>
2020-01-28 21:43:57 +0800

    Update 3_longest_substring_with_k_distinct.py

--- 1_sliding_window/3_longest_substring_with_k_distinct.py
+++ 1_sliding_window/3_longest_substring_with_k_distinct.py
@@ -21,21 +21,24 @@ def longest_substring_with_k_distinct(str, k):
 
 
 def longest_substring_with_k_distinct2(str, k):
-    start = 0
-    max_len = -math.inf
     dic = {}
+    start = 0
+    length = 0
+
     for end in range(len(str)):
         s = str[end]
+
         dic[s] = dic.get(s, 0) + 1
 
-        while len(dic) > k:
-            left_s = str[start]
-            dic[left_s] -= 1
-            if dic[left_s] == 0:
-                del dic[left_s]
+        if len(dic) > k:
+            dic[str[start]] -= 1
+            if dic[str[start]] == 0:
+                del dic[str[start]]
             start += 1
-        max_len = max(max_len, end - start + 1)
-    return max_len
+
+        length = max(length, end - start + 1)
+
+    return length
 
 
 def main():
