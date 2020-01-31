3e7b87dad5e72dad0be192f30809a1a1ce269962
terrifyzhao <zjiuzhou@gmail.com>
2020-01-31 22:32:48 +0800

    update code

--- 9_two_heaps/1_find_median.py
+++ 9_two_heaps/1_find_median.py
@@ -23,6 +23,29 @@ class MedianOfAStream:
         return -self.max_heap[0]
 
 
+class MedianOfAStream2:
+    max_heap = []
+    min_heap = []
+
+    def insert_num(self, num):
+        if not self.max_heap or num <= -self.max_heap[0]:
+            heappush(self.max_heap, -num)
+        else:
+            heappush(self.min_heap, num)
+
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heappush(self.min_heap, -heappop(self.max_heap))
+
+        if len(self.min_heap) > len(self.max_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
+
+    def find_median(self):
+        if len(self.max_heap) == len(self.min_heap):
+            return (-self.max_heap[0] + self.min_heap[0]) / 2
+        else:
+            return -self.max_heap[0]
+
+
 def main():
     medianOfAStream = MedianOfAStream()
     medianOfAStream.insert_num(3)
--- 9_two_heaps/2_sliding_window_median.py
+++ 9_two_heaps/2_sliding_window_median.py
@@ -52,6 +52,56 @@ class SlidingWindowMedian:
             # heapq.heapify(heap)
 
 
+class SlidingWindowMedian:
+
+    def __init__(self):
+        self.max_heap = []
+        self.min_heap = []
+
+    def find_sliding_window_median(self, nums, k):
+        result = []
+
+        for i in range(len(nums)):
+
+            if not self.max_heap or nums[i] <= -self.max_heap[0]:
+                heappush(self.max_heap, -nums[i])
+            else:
+                heappush(self.min_heap, nums[i])
+
+            self.balance()
+
+            if i >= k - 1:
+                if len(self.max_heap) == len(self.min_heap):
+                    result.append((-self.max_heap[0] + self.min_heap[0]) / 2)
+                else:
+                    result.append(-self.max_heap[0])
+
+                element = nums[i - k + 1]
+                if element <= -self.max_heap[0]:
+                    self.remove(self.max_heap, -element)
+                else:
+                    self.remove(self.min_heap, element)
+                self.balance()
+
+        return result
+
+    def balance(self):
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heappush(self.min_heap, -heappop(self.max_heap))
+
+        if len(self.min_heap) > len(self.max_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
+
+    def remove(self, heap, num):
+        idx = heap.index(num)
+        heap[idx] = heap[-1]
+        heap.pop()
+
+        if idx < len(heap):
+            heapq._siftup(heap, idx)
+            heapq._siftdown(heap, 0, idx)
+
+
 def main():
     slidingWindowMedian = SlidingWindowMedian()
     result = slidingWindowMedian.find_sliding_window_median(
